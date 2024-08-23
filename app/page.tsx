ecb01c708f33ead1b9b4fc6641705f0eb1975cba
Ninja AZ <lovepinklol123@gmail.com>
2024-08-22 21:06:13 -0700

    initial files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <div className="text-4xl mt-[20vh]">
+        Welcome to Core Machine Learning Group
+      </div>
+    </div>
+  );
+}
